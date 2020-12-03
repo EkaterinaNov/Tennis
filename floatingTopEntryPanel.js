e7e4d6f565ff13cff95a81223e0360a780d52e08
EkaterinaNov <ekaterinagnv@gmail.com>
2020-12-03 23:02:31 +0300

    redact validEntryPanel

--- floatingTopEntryPanel.js
+++ floatingTopEntryPanel.js
@@ -2,12 +2,14 @@
 (function() {
     const entryPanelClass = 'entryPanel';
     const entryPanelElement = document.getElementsByClassName(entryPanelClass)[0];
+    const gamePanelClass = 'gamePanel';
+    const gamePanelElement = document.getElementsByClassName(gamePanelClass)[0];
 
-    function floatingOnTopCenter (animatedElement) {
-        const bodyWidth = document.body.offsetWidth;
-        const animatedElementWidth = animatedElement.offsetWidth;
-        const animatedElementHeight = animatedElement.offsetHeight;
-        const startX = (bodyWidth / 2) - (animatedElementWidth / 2);
+    function floatingOnTopCenter (animatedElement, parentElement) {
+        const parentElementWidth = parseFloat(parentElement.offsetWidth);
+        const animatedElementWidth = parseFloat(animatedElement.offsetWidth);
+        const animatedElementHeight = parseFloat(animatedElement.offsetHeight);
+        const startX = (parentElementWidth / 2) - (animatedElementWidth / 2);
         const startY = -animatedElementHeight - 10;
         const endX = startX;
         const endY = 20;
@@ -22,10 +24,10 @@
             delay: 1000
         };
 
+        animatedElement.style.transform = `translate3D(${startX}px, ${startY}px, 0px)`;
         animatedElement.animate(keyframes, options);
-        animatedElement.style.left = endX + 'px';
-        animatedElement.style.top = endY + 'px';
+        animatedElement.style.transform = `translate3D(${endX}px, ${endY}px, 0px)`;
     }
 
-    floatingOnTopCenter(entryPanelElement);
+    floatingOnTopCenter(entryPanelElement, gamePanelElement);
 })()
--- validEntryPanel.js
+++ validEntryPanel.js
@@ -1,11 +1,79 @@
 'use strict';
 (function() {
-    function validEntryPanel(EO) {
-        const notificationElementClass = 'notificationLabel';
-        const EMPTY_STRING = '';
-        const entryPanelForm = document.forms.entryPanel;
+    function validFirstPlayer(isFocus) {
+        var errorCounter = 0;
+        var validatedElement = document.forms.entryPanel.elements.firstPlayer;
 
-        function validFirstPlayer(form)
+        if(!validatedElement.value) {
+            errorCounter++;
+            if(isFocus) {
+                validatedElement.focus();
+            }
+        }
+        return errorCounter;
     }
-    
+    function validSecondPlayer(isFocus) {
+        var errorCounter = 0;
+        var validatedElement = document.forms.entryPanel.elements.secondPlayer;
+
+        if(!validatedElement.value) {
+            errorCounter++;
+            if(isFocus) {
+                validatedElement.focus();
+            }
+        }
+        return errorCounter;
+    }
+
+    function validateEntryPanel(EO) {
+        EO=EO||window.event;
+
+        try {
+            const entryPanelClass = 'entryPanel';
+            const entryPanelElement = document.getElementsByClassName(entryPanelClass)[0];
+            const notificationElementClass = 'notificationLabel';
+            const notificationElement = document.getElementsByClassName(notificationElementClass)[0];
+            const visibilityVisible = 'visible';
+            const visibilityHidden = 'hidden';
+            var numberMissed = 0;
+
+            numberMissed += validFirstPlayer(!numberMissed);
+            numberMissed += validSecondPlayer(!numberMissed);
+
+            if(numberMissed) {
+                notificationElement.style.visibility = visibilityVisible;
+                EO.preventDefault();
+            } else {
+                notificationElement.style.visibility = visibilityHidden;
+                changeFirstPlayerName();
+                changeSecondPlayerName();
+                entryPanelElement.style.transform = '';
+                EO.preventDefault();
+            }
+        }
+
+        catch {
+            alert('Извините, что-то пошло не так. Проверьте правильность заполнения формы.');
+            EO.preventDefault();
+        }
+        return;
+    }
+
+    function changeFirstPlayerName() {
+        const pastNameElementClass = 'firstPlayerName';
+        var firstPlayerNameValue = document.forms.entryPanel.elements.firstPlayer.value;
+        var pastNameElement = document.getElementsByClassName(pastNameElementClass)[0];
+        pastNameElement.textContent = firstPlayerNameValue;
+    }
+
+    function changeSecondPlayerName() {
+        const pastNameElementClass = 'secondPlayerName';
+        var secondPlayerNameValue = document.forms.entryPanel.elements.secondPlayer.value;
+        var pastNameElement = document.getElementsByClassName(pastNameElementClass)[0];
+        pastNameElement.textContent = secondPlayerNameValue;
+    }
+
+    const buttonFormFieldSelector = 'button.formField';
+    const buttonFormFieldElement = document.querySelector(buttonFormFieldSelector);
+    buttonFormFieldElement.addEventListener("click", validateEntryPanel);
 })()
